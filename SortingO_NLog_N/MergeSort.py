a1a3ae705c719bbf1322dc2cc26b0f2322b4ed9b
MostafaAdel-cs <es-mostafaadel2023@alexu.edu.eg>
2021-05-12 14:18:14 +0200

    Merge Sort Completed

--- SortingO_NLog_N/MergeSort.py
+++ SortingO_NLog_N/MergeSort.py
@@ -18,10 +18,10 @@ def MergeSort(theList: list, first, last):
     return None
 
 
-# argument first is the location where the sub array is inserted in the original array
-def merge(left: list, right: list, theList: list, first):
+# argument locationOfInsertion if not given the arrays will be merged in index 0 of original array
+def merge(left: list, right: list, theList: list, locationOfInsertion=0):
     nL, nR = len(left), len(right)
-    i, j, k = 0, 0, first
+    i, j, k = 0, 0, locationOfInsertion
     while i < nL and j < nR:
         if left[i] <= right[j]:
             theList[k], i, k = left[i], i + 1, k + 1
--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import time
 from SortingO_N2 import InsertionSort
 from SortingO_NLog_N import MergeSort
 import sys
+def mos(k,optional_argument=1):
+    print(k)
+    print(optional_argument)
+    return None
 
 p=[6,3,9,1,5,4,7,2]
-l=[1,3,6,9]
-r=[2,4,5,7]
-z=[1,2,3,4,5,6,7,8]
-
 MergeSort.sortArray(p)
-print('====================')
-for i in p:
-    print(i)
+
+print(p)
+
+
