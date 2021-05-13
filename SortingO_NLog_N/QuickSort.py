586e75437187e470478f4dd9882a29550612a004
MostafaAdel-cs <es-mostafaadel2023@alexu.edu.eg>
2021-05-13 23:51:17 +0200

    Done
    Added Iterative quick sort

--- SortingO_NLog_N/QuickSort.py
+++ SortingO_NLog_N/QuickSort.py
@@ -1,21 +1,61 @@
-def sortArray(theList: list):
+import random
+
+
+def sortArrayRecursively(theList: list):
     quickSort(theList, 0, len(theList) - 1)
     return None
 
 
 def quickSort(theList: list, first, last):
     if first < last:
-        q = partition(theList, first, last)
+        q = randomPartition(theList, first, last)
         quickSort(theList, first, q - 1)
         quickSort(theList, q + 1, last)
+    return None
+
+
+def sortArrayIteratively(theList: list):
+    iterativeQuickSort(theList, 0, len(theList) - 1)
+    return None
+
+
+def iterativeQuickSort(theList: list, first, last):
+    stack = [None] * len(theList)
+    # Allocating stack size
+    top = -1
+    stack.append(first)
+    stack.append(last)
+    top = top + 2
+    while top >= 0:
+
+        l = stack.pop()
+        f = stack.pop()
+        top = top - 2
+        p = partition(theList, f, l)
+
+        if p - 1 > f:
+            stack.append(first)
+            stack.append(p - 1)
+            top = top + 2
+        if p + 1 < l:
+            stack.append(p + 1)
+            stack.append(last)
+            top = top + 2
+    return None
+
+
+def randomPartition(theList: list, first, last):
+    i = random.randint(first, last)
+    theList[last], theList[i] = theList[i], theList[last]
+    return partition(theList, first, last)
 
 
 def partition(theList: list, first, last):
-    lastS1 = first
-    firstUnknown = first + 1
-    while firstUnknown <= last:
-        if theList[firstUnknown] < theList[first]:
-            theList[firstUnknown], theList[lastS1] = theList[lastS1], theList[firstUnknown]
-        firstUnknown = firstUnknown + 1
-    theList[first], theList[lastS1] = theList[lastS1], theList[first]
-    return lastS1
+    pivot = theList[last]
+    i = first - 1
+    for j in range(first, last):
+        if theList[j] <= pivot:
+            i = i + 1
+            theList[i], theList[j] = theList[j], theList[i]
+    theList[i + 1], theList[last] = theList[last], theList[i + 1]
+    return i + 1
--- main.py
+++ main.py
@@ -1,16 +1,61 @@
 import time
+from SortingO_N2 import BubbleSort
 from SortingO_N2 import InsertionSort
+from SortingO_N2 import SelectionSort
 from SortingO_NLog_N import Heap
-import heapq
+from SortingO_NLog_N import MergeSort
+from SortingO_NLog_N import QuickSort
 import sys
+import numpy as np
 
+# adjust n to for different array sizes
+n = 1000
+randNums = np.random.randint(0, 100, n)
 
-p=[1, 14, 10, 8, 7, 9, 3, 2, 4, 16]
-z=[16,4,10,14,7,9,3,2,8,1]
+print('O(n^2) Algorithms times:')
+theList = randNums.copy()
+start = time.time()
+BubbleSort.sortArray(theList)
+end = time.time()
+print(f'\tBubble Sort: {end - start}')
 
-Heap.heapSort(p)
+theList = randNums.copy()
+start = time.time()
+InsertionSort.sortArray(theList)
+end = time.time()
+print(f'\tInsertion Sort: {end - start}')
 
-print(p)
+theList = randNums.copy()
+start = time.time()
+SelectionSort.sortArray(theList)
+end = time.time()
+print(f'\tSelection Sort: {end - start}')
 
+print('======================================================')
 
+print('O(n*log(n)) Algorithms times:')
+
+theList = randNums.copy()
+start = time.time()
+Heap.heapSort(theList)
+end = time.time()
+print(f'\tHeap Sort: {end - start}')
+
+theList = randNums.copy()
+start = time.time()
+MergeSort.sortArray(theList)
+end = time.time()
+print(f'\tMerge Sort: {end - start}')
+
+theList = randNums.copy()
+start = time.time()
+QuickSort.sortArrayRecursively(theList)
+end = time.time()
+print(f'\tQuick Recursively Sort: {end - start}')
+
+theList = randNums.copy()
+start = time.time()
+QuickSort.sortArrayIteratively(theList)
+end = time.time()
+print(f'\tQuick Iteratively Sort: {end - start}')
 
